b6e4a5afc6b20f12131346489c96425bc263936b
Mercy <mercywmuriithi.mm@gmail.com>
2023-09-23 12:43:36 +0300

    total amount calculated

--- main.py
+++ main.py
@@ -31,17 +31,44 @@
 #An instance is an indiv object
 
 class Bank_Accounts:
+    total_balance = 0
     def __init__(self,acc_number,acc_name,balance): 
         self.acc_number=acc_number
         self.acc_name=acc_name
         self.balance=balance
+        Bank_Accounts.total_balance += self.balance
+
+    @classmethod
+    def total_bank_balance(cls):
+            print(cls.total_balance)
 
     def deposit(self,amount):
         self.balance+=amount
+        Bank_Accounts.total_balance += amount
     
     def check_balance(self):
         print(f'Hello {self.acc_name}, Your current balance is ${self.balance}')
+
+    
+    def withdraw(self,amount):
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            print(f'Hello {self.acc_name},You have successfully withdrawn Ksh {amount}. Your current balance is {self.balance}')
+        else:
+            print("Insufficient balance")
+
+
+class Savings_account(Bank_Accounts):
+        
+        def add_interest(self):
+            self.balance *= 1.03
 #object
 mary=Bank_Accounts(12242303,'Mary',700)
 mary.deposit(200)
 mary.check_balance()
+
+Bin = Bank_Accounts(122334422,"Bin Amin",0)
+Dan = Bank_Accounts("122223334423","Danwycliff",5000)
+Bin.deposit(60000)
+
+Bank_Accounts.total_bank_balance()
